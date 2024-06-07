30e0853be4b2c8f1efc11b0e0a2e099b4519a146
soocrates <rooshan.vtech@gmail.com>
2024-06-07 11:32:13 +0545

    first commit

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+/* Database credentials from environment variables */
+define('DB_SERVER', getenv('DB_SERVER'));
+define('DB_USERNAME', getenv('DB_USERNAME'));
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
+define('DB_NAME', getenv('DB_NAME'));
+
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
